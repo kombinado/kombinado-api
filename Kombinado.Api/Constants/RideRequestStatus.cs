ba6f29d9fca77cf80e34d692c9c3a3ce77702cf8
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-13 20:05:01 -0300

    FEAT: Create the RideRequestStatus constant class

---
+++ Kombinado.Api/Constants/RideRequestStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Kombinado.Api.Constants
+{
+    public static class RideRequestStatus
+    {
+        public const string Pending = "Pendente";
+        public const string Accepted = "Aceita";
+        public const string Rejected = "Recusada";
+    }
+}
