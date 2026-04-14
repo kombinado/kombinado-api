25c4f2fbc3cf0623b646c7e858a096d0590468ec
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-13 22:03:03 -0300

    FEAT: Create the SignupRequestDto model

---
+++ Kombinado.Api/Models/DTOs/Requests/SignupRequestDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Kombinado.Api.Models.DTOs.Requests
+{
+    public class SignupRequestDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string WhatsApp { get; set; } = string.Empty;
+        public string Course { get; set; } = string.Empty;
+        public bool IsDriver { get; set; }
+
+        // Vehicle data (only for drivers)
+        public string? VehicleModel { get; set; }
+        public string? VehicleColor { get; set; }
+        public string? VehiclePlate { get; set; }
+    }
+}
