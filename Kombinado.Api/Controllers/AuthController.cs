663229e0bf5f5f04f3c096304e55d232ce612dd9
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-13 22:07:23 -0300

    FEAT: Create the AuthController class

---
+++ Kombinado.Api/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Kombinado.Api.Models.DTOs.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kombinado.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("signup")]
+        public IActionResult Signup([FromBody] SignupRequestDto request)
+        {
+            return Ok("Por enquanto ainda não tem, mas ta chegando rapaze!");
+        }
+    }
+}
