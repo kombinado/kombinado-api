6be447e44ea6ee8296efc813da836871f2349d2d
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-20 15:56:02 -0300

    FEAT: Create the LoginResponseDto class

---
+++ Kombinado.Api/Models/DTOs/Responses/LoginResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Kombinado.Api.Models.DTOs.Responses
+{
+    public class LoginResponseDto
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public bool IsDriver { get; set; }
+    }
+}
