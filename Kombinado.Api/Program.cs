096ff5a031623bba9b0e166ad89d891c9d383120
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-20 17:21:56 -0300

    FEAT: Add authentication middleware in Program.cs

--- Kombinado.Api/Program.cs
+++ Kombinado.Api/Program.cs
@@ -1,8 +1,13 @@
 using Kombinado.Api.Data;
 using Kombinado.Api.Services.Auth;
 using Kombinado.Api.Services.Token;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
+// Load environment variables from the .env file
 DotNetEnv.Env.Load(Path.Combine(Directory.GetCurrentDirectory(), "..", ".env"));
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -15,14 +20,37 @@ builder.Services.AddDbContext<KombinadoDbContext>(o => o.UseNpgsql(connectionStr
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ITokenService, TokenService>();
 
+// JWT Authentication configuration
+string? secretKey = builder.Configuration["JWT_SECRET"] 
+    ?? throw new InvalidOperationException("JWT_SECRET não configurado.");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.MapInboundClaims = false;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JWT_ISSUER"],
+        ValidAudience = builder.Configuration["JWT_AUDIENCE"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+builder.Services.AddAuthorization();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddControllers();
 
 WebApplication app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -30,6 +58,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Create a root endpoint
 app.Map("/", () => "Welcome to Kombinado API!");
