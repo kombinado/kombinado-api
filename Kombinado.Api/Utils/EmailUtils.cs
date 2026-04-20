a63cb05725acbf617e9d848fe9749017e6f7016d
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-20 14:42:35 -0300

    FEAT: Create the EmailUtils static class

---
+++ Kombinado.Api/Utils/EmailUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Kombinado.Api.Utils
+{
+    public static class EmailUtils
+    {
+        private const string STUDENT_EMAIL_REGEX = @"^[\w-\.]+@estudante\.iftm\.edu\.br$";
+
+        public static bool IsStudentEmail(string email)
+        {
+            return Regex.IsMatch(email, STUDENT_EMAIL_REGEX);
+        }
+    }
+}
