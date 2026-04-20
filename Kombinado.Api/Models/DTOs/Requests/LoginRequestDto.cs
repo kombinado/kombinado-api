b1fbc4ca56731ce2a0e09dbd68ba635ad2f95753
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-20 15:44:36 -0300

    FEAT: Create the LoginRequestDto class

---
+++ Kombinado.Api/Models/DTOs/Requests/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Kombinado.Api.Models.DTOs.Requests
+{
+    public class LoginRequestDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
