953faf7e276ffb8f7938008ffb2f836110cd64fa
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-20 16:16:13 -0300

    FEAT: Create the ITokenService interface

---
+++ Kombinado.Api/Services/Token/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using Kombinado.Api.Models.Entities;
+using System.Security.Claims;
+
+namespace Kombinado.Api.Services.Token
+{
+    public interface ITokenService
+    {
+        string GenerateAccessToken(UserEntity user);
+
+        string GenerateRefreshToken();
+
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+    }
+}
