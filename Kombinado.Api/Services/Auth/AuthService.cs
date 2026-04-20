015f20c552aba8bc7ad9fadca35564b3bc764df6
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-20 14:53:47 -0300

    FEAT: Create the AuthService class

---
+++ Kombinado.Api/Services/Auth/AuthService.cs
@@ -0,0 +1,53 @@
+﻿using Kombinado.Api.Data;
+using Kombinado.Api.Models;
+using Kombinado.Api.Models.DTOs.Requests;
+using Kombinado.Api.Models.Entities;
+using Kombinado.Api.Utils;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kombinado.Api.Services.Auth
+{
+    public class AuthService : IAuthService
+    {
+        private readonly KombinadoDbContext _dbContext;
+        public AuthService(KombinadoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<ApiResponse<string>> SignupAsync(SignupRequestDto request)
+        {
+            // 1. Check if email is student email
+            if (!EmailUtils.IsStudentEmail(request.Email))
+            {
+                return ApiResponse<string>.FailureResponse("É necessário um e-mail institucional do IFTM (@estudante.iftm.edu.br).", 400);
+            }
+
+            // 2. Check if email is already registered
+            if (await _dbContext.Users.AnyAsync(u => u.Email == request.Email))
+            {
+                return ApiResponse<string>.FailureResponse("Este e-mail já está cadastrado.", 400);
+            }
+
+            // 3. Create new user
+            UserEntity user = new UserEntity
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Email = request.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password),
+                WhatsApp = request.WhatsApp,
+                Course = request.Course,
+                IsDriver = request.IsDriver,
+                VehicleModel = request.IsDriver ? request.VehicleModel : null,
+                VehicleColor = request.IsDriver ? request.VehicleColor : null,
+                VehiclePlate = request.IsDriver ? request.VehiclePlate : null
+            };
+
+            _dbContext.Users.Add(user);
+            await _dbContext.SaveChangesAsync();
+            
+            return ApiResponse<string>.SuccessResponse("Cadastro realizado com sucesso!");
+        }
+    }
+}
