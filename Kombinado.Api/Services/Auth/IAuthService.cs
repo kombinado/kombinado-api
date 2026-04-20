b5fb9338e10835836a2bef4e60bbc5bf227e7ec4
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-20 14:35:38 -0300

    FEAT: Create the IAuthService interface

---
+++ Kombinado.Api/Services/Auth/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Kombinado.Api.Models;
+using Kombinado.Api.Models.DTOs.Requests;
+
+namespace Kombinado.Api.Services.Auth
+{
+    public interface IAuthService
+    {
+        Task<ApiResponse<string>> SignupAsync(SignupRequestDto request);
+    }
+}
