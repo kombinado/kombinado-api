f0e1866e7b11691d8917d844367d9b52a07be4f3
DevLuquinha <lucasemmanuelrodrigues737@gmail.com>
2026-05-17 17:05:40 -0300

    FEAT: Create the RidesController class

---
+++ Kombinado.Api/Controllers/RidesController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kombinado.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RidesController : ControllerBase
+    {
+    
+    }
+}
