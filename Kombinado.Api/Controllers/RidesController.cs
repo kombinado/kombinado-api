30c5580ec9eaf08e50605465a2d15a73db72e96b
DevLuquinha <lucasemmanuelrodrigues737@gmail.com>
2026-05-17 18:21:24 -0300

    FEAT: Implement CreateRide endpoint in RidesController with driver authorization

--- Kombinado.Api/Controllers/RidesController.cs
+++ Kombinado.Api/Controllers/RidesController.cs
@@ -1,11 +1,36 @@
+using Kombinado.Api.Extensions;
+using Kombinado.Api.Models.DTOs.Requests;
+using Kombinado.Api.Services.Ride;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kombinado.Api.Controllers
 {
+    [Authorize] 
     [ApiController]
     [Route("api/[controller]")]
     public class RidesController : ControllerBase
     {
-    
+        private readonly IRideService _rideService;
+
+        public RidesController(IRideService rideService)
+        {
+            _rideService = rideService;
+        }
+
+        [HttpPost]
+        [Authorize("DriverOnly")]
+        public async Task<IActionResult> CreateRide([FromBody] CreateRideRequestDto request)
+        {
+            Guid driverId = User.GetUserId();
+            
+            var response = await _rideService.CreateRideAsync(request, driverId);
+            if (!response.Success)
+            {
+                return StatusCode(response.StatusCode, response);
+            }
+            
+            return StatusCode(201, response);
+        }
     }
 }
